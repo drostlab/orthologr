0265ca1d534743be5f5dbe521404450205068fbf
bitbarbie <sarah.scharfenberg.halle@gmail.com>
2014-09-15 00:21:30 +0200

    Addig - funcs_alignment_tools.R including multi_aln() a wrapper to use console based multiple aligning tools like clustalw, tcoffe, clustalo and muscle. - funcs_pal2nal.R including codon_aln() a wrapper to use console based tools to create codon alignment as pal2nal.

---
+++ R/funcs_alignment_tools.R
@@ -0,0 +1,61 @@
+#' @title Function to get a multiple alignment.
+#' @description This function takes a multiple FASTA file containing DNA or aminoacid sequences
+#' to be aligned and proceeds the method defined by mode to align them.
+#' @param file a character string specifying the path to the file storing the sequences in FASTA format.
+#' @param file.out a character string specifying the path where the alignment should be stored.
+#' @param mode a character string specifying the program that should be used e.g. "clustalw". 
+#' @param get.aln a logical value indicating whether the produced alignment should be returned.
+#' @author Sarah Scharfenberg and Hajk-Georg Drost 
+#' @details 
+#' @return if get.aln is TRUE an object of class alignment of the seqinr package.
+#' @export
+multi_aln<-function(file, file.out, mode, get.aln="FALSE"){
+        
+        if(!is.element(mode,c("clustalw", "tcoffee", "muscle", "clustalo")))
+                stop("Please choose a mode that is supported by this function.")
+        
+        if(mode=="clustalw"){
+        
+                system(paste0("clustalw ",file," -outfile=",file.out," -quiet"))
+
+                if(get.aln){
+                        aln <- seqinr::read.alignment(file.out, format="clustal")
+                        return(aln)
+                }
+        }
+ 
+        if(mode=="tcoffee"){
+                
+                system(paste0("t_coffee ",file," >",file.out))
+                
+                if(get.aln){
+                        aln <- seqinr::read.alignment(file.out, format="clustal")
+                        return(aln)
+                }
+        }
+ 
+         if(mode=="muscle"){
+                 
+                 system(paste0("muscle -in ",file," -out ",file.out))
+                 
+                 if(get.aln){
+                         aln <- seqinr::read.alignment(file.out, format="fasta")
+                         return(aln)
+                 }
+         }
+ 
+         if(mode=="clustalo"){
+                
+                 system(paste0("clustalo -i ",file," -o ",file.out," --outfmt clustal"))
+                 
+                 if(get.aln){
+                         aln <- seqinr::read.alignment(file.out, format="clustal")
+                         return(aln)
+                 }
+         }
+
+}
+
+
+
+
---
+++ R/funcs_pal2nal.R
@@ -0,0 +1,33 @@
+#' @title Function to get a codon alignment.
+#' @description This function takes a protein alignment and the corresponding coding sequences 
+#' both given as files and uses the method specified by mode to create the corresponding codon
+#' alignment.
+#' @param file.aln a character string specifying the path to the file storing the protein alignment in CLUSTAL or FASTA format.
+#' @param file.nuc a character string specifying the path to the file storing the coding sequences in multiple FASTA format.
+#' @param file.out a character string specifying the path where the alignment should be stored.
+#' @param format a character string specifying the file format used to store the codon alignment, e.g. "fasta", "clustal".
+#' @param mode a character string specifying the program that should be used e.g. "pal2nal". 
+#' @param get.aln a logical value indicating whether the produced alignment should be returned.
+#' @param path a character string specifiying the absolute path where pal2nal.pl can be found.
+#' @author Sarah Scharfenberg and Hajk-Georg Drost 
+#' @details 
+#' @return if get.aln is TRUE an object of class alignment of the seqinr package.
+#' @export
+codon_aln<-function(file.aln, file.nuc, file.out, format="clustal", mode, get.aln=FALSE, path){
+                
+        if(!is.element(mode,c("pal2nal")))
+                stop("Please choose a mode that is supported by this function.")
+        
+        if(!is.element(format,c("clustal", "fasta")))
+                stop("Please choose a format that is supported by this function.")
+        
+        if(mode=="pal2nal"){
+                system(paste0(path,"pal2nal.pl ",file.aln," ",file.nuc," >",file.out))
+                
+                if(get.aln){
+                        dna_aln <- seqinr::read.alignment(file=file.out, format="clustal")
+                        return(dna_aln)
+                }
+        }
+}
+
