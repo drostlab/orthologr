6b42973b14f714677beea4115eaa2af51bfa802e
bitbarbie <sarah.scharfenberg.halle@gmail.com>
2014-09-22 20:25:28 +0200

    Fixed comma error in multi_aln().

--- R/multi_aln.R
+++ R/multi_aln.R
@@ -237,7 +237,7 @@
 multi_aln <- function(file, tool, get_aln = FALSE, path = NULL,
                       clustalw.params = NULL, t_coffee.params = NULL,
                       muscle.params = NULL, clustalo.params = NULL,
-                      mafft.params = NULL
+                      mafft.params = NULL,
                       quiet=FALSE){
         
         if(!is.element(tool,c("clustalw", "t_coffee", "muscle", "clustalo","mafft")))
