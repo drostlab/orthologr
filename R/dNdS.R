779ee0d1393646454b48cbed42df3147edcc991d
bitbarbie <sarah.scharfenberg.halle@gmail.com>
2014-09-24 13:27:47 +0200

    Changed tcoffee  -> t_coffee in dNdS().

--- R/dNdS.R
+++ R/dNdS.R
@@ -23,7 +23,7 @@ dNdS <- function(query_file, subject_file,
         if(!is.element(blast_mode, c("best hit","recursive")))
                 stop("Please choose a blast mode that is supported by this function.")
         
-        if(!is.element(multialn_tool, c("clustalw", "clustalo","muscle", "tcoffee", "mafft")))
+        if(!is.element(multialn_tool, c("clustalw", "clustalo","muscle", "t_coffee", "mafft")))
                 stop("Please choose a multiple alignment tool that is supported by this function.")
         
         if(!is.element(codonaln_tool, c("pal2nal")))
