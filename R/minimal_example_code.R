5d2924fc98fbf7af8a6fe2eb16bda86dc0bf3724
HajkD <hajkdrost@googlemail.com>
2014-09-26 09:44:06 +0200

    changing the minimal example

--- R/minimal_example_code.R
+++ R/minimal_example_code.R
@@ -1,6 +1,6 @@
 
 # example to check tcoffee run -> much output that we should try to eliminate
-#aln <- multi_aln(file = "data/aa_seqs.fasta", tool = "tcoffee", get_aln = TRUE, path = "/Users/Hajk/Desktop/Projekte/bioinformatics tools/t_coffee/bin")
+#aln <- multi_aln(file = "data/aa_seqs.fasta", tool = "tcoffee", get_aln = TRUE, path = "")
 
 #example to check pal2nal run -> use your own path to pal2nal
 # codon_aln <- codon_aln(file_aln = system.file('seqs/aa_seqs.aln', package = 'orthologr'),
@@ -8,12 +8,12 @@
 #                        format = "clustal", tool = "pal2nal", get_aln = TRUE)
 
 
-dNdS(query_file = system.file('seqs/ortho_thal_cds.fasta', package = 'orthologr'),
-     subject_file = system.file('seqs/ortho_lyra_cds.fasta', package = 'orthologr'),
-     ortho_detection = "RBH", blast_path = NULL,
-     multialn_tool = "t_coffee", multialn_path = "/Users/Hajk/Desktop/Projekte/bioinformatics_tools/t_coffee/bin",
-     codonaln_tool = "pal2nal", dnds_est.method = "YN", comp_cores = 2, quiet = FALSE)
-# 
+# dNdS(query_file = system.file('seqs/ortho_thal_cds.fasta', package = 'orthologr'),
+#      subject_file = system.file('seqs/ortho_lyra_cds.fasta', package = 'orthologr'),
+#      ortho_detection = "RBH", blast_path = NULL,
+#      multialn_tool = "t_coffee", multialn_path = "",
+#      codonaln_tool = "pal2nal", dnds_est.method = "YN", comp_cores = 2, quiet = FALSE)
+# # 
 # 
 # compute_dnds <- function(x, 
 #                          multialn_tool="clustalw", multialn_path = NULL,
