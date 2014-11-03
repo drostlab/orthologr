71bac1ead45ead0e7cf92298e620529d104db0bf
bitbarbie <sarah.scharfenberg.halle@gmail.com>
2014-11-03 12:02:01 +0100

    Adding --anysymbol to mafft default params to avoid deleting * during the alignenment.

--- R/multi_aln.R
+++ R/multi_aln.R
@@ -539,7 +539,11 @@ multi_aln <- function(file, tool, get_aln = FALSE, path = NULL,
                          if(is.null(mafft.params)){
                                 
                                 # use the default parameters when running mafft
-                                system(paste0("mafft --quiet --clustalout ",file," >",file.out))
+                                system(paste0("mafft --quiet --amino --anysymbol --clustalout ",file," >",file.out))
+                                
+                                # To avoid error with pal2nal when deleting * during multiple
+                                # alignment add --anysymbol
+                                # http://mbe.oxfordjournals.org/content/early/2013/02/08/molbev.mst010.full
                                 
                          } else {
                                 
