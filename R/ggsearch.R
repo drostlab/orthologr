a104712fe11d88f6d4ea59d565d2874546529b5e
HajkD <hajkdrost@googlemail.com>
2014-12-11 11:19:53 +0100

    updating documentation of function ggsearch()

--- R/ggsearch.R
+++ R/ggsearch.R
@@ -1,57 +1,70 @@
-#' @title Interface to the commandline tool ggsearch36.
-#' @description This function is a wrapper for the tool ggsearch that performs
-#' global alignment for each sequence in the inputfile with each sequence in the 
-#' library and returns the alignment with the best score.
-#' @param file a character string specifying the path to the sequence file.
-#' @param library_file a character string specifying the path to the library file.
-#' @param lib_format a character string specifying the file type of the library e.g. fasta.
-#' @param options a character string specifying additional options for ggsearch
-#' @param seq_type a character string specifying the type of the sequences
-#' @param path a character string specifying the path to ggsearch if not included in 
-#' the global environment
-#' @param parse_output_to a charcater string specifying a file where the output of
-#' ggsearch is parsed to
-#' @details "Perform a rigorous Global/Global (GGSEARCH) [...] alignment between 
-#' a protein sequence and another protein sequence or a protein database [or
-#' a DNA sequence to a DNA sequence database ]. GGSEARCH implements a Needleman-Wunsch like alignment, except that affine gap 
-#' penalties are used. GLSEARCH is most appropriate for global searches with a 
-#' domain, which require local alignments within proteins. 
+#' @title Perform Global Alignments with GGSearch 
+#' @description This function allows you to perform global alignments between pairs
+#' of protein sequences or alignments between a protein sequence against a 
+#' protein database based on the GGSeach program implemented in FASTA framework. 
+#' 
+#' @param file a character string specifying the path to the query sequence file.
+#' @param library_file a character string specifying the path to the subject library file.
+#' @param lib_format a character string specifying the file type of the library e.g. \code{lib_format} = \code{"fasta"}.
+#' @param options a character string specifying additional options for GGSearch.
+#' @param seq_type a character string specifying the type of biological sequences.
+#' @param path a character string specifying the path to GGSearch. This argument only needs
+#' to be specified when GGSearch cannot be executed from the default execution \code{PATH} (global environment).
+#' @param parse_output_to a charcater string specifying a filename in which the output of
+#' GGSearch is written.
+#' @details 
+#' 
+#' This function takes two fasta files representing query and subject (library) sequences.
+#' 
+#' As a result, this function returns the alignment score and additional parameters
+#' computed by GGSearch. GGSearch implements a Needleman-Wunsch like alignment, except that affine gap 
+#' penalties are used. 
+#' 
 #' @author Sarah Scharfenberg and Hajk-Georg Drost
-#' @references ggsearch is part of the fasta36 package that can be found here 
-#' http://fasta.bioch.virginia.edu/fasta_www2/fasta_down.shtml
+#' @references 
+#' 
+#' GGSearch is part of the fasta36 package that can be downloaded here 
+#' 
+#' \url{http://fasta.bioch.virginia.edu/fasta_www2/fasta_down.shtml}
+#' 
 #' @examples \dontrun{
-#' # performing ggsearch on two protein files
+#' 
+#' # performing GGSearch on two protein files
 #' ggsearch( file = system.file('seqs/ortho_thal_aa.fasta', package = 'orthologr'),
 #'           library_file = system.file('seqs/ortho_lyra_aa.fasta', package = 'orthologr'))
 #'   
-#' # performing ggsearch on two cds files parsing the output                   
+#' # performing GGSearch on two cds files parsing the output                   
 #' ggsearch( file = system.file('seqs/ortho_thal_cds.fasta', package = 'orthologr'),
 #'           library_file = system.file('seqs/ortho_lyra_cds.fasta', package = 'orthologr'),
-#'           seqtype="cds", parse_output_to = "ggsearch.out")
+#'           seqtype = "cds", parse_output_to = "ggsearch.out")
 #'               
 #' }
 #' @export
-ggsearch <- function(file, library_file, options="", lib_format="fasta",
-                     seq_type = "protein",
-                     path=NULL, parse_output_to=NULL){
+
+ggsearch <- function(file, library_file, options = "", lib_format = "fasta",
+                     seq_type = "protein", path = NULL, parse_output_to = NULL){
         
         if(seq_type == "protein"){
+                
                 options <- paste0(options, "-p")
         }
         
         operating_sys <- Sys.info()[1]
         
         if (operating_sys == "Darwin"){ 
+                
                 os <- "Mac"
                 calc <- "ggsearch36"
         }
         
         if (operating_sys == "Linux"){
+                
                 os <- "Linux"
                 calc <- "ggsearch36"
         }
         
         if (operating_sys == "Windows"){ 
+                
                 os <- "Windows"
                 calc <- "ggsearch36.exe"
         }
@@ -60,19 +73,35 @@ ggsearch <- function(file, library_file, options="", lib_format="fasta",
         # differentiate system call
         
         if(is.null(path)){
+                
                 if(is.null(parse_output_to)){
+                        
                         system(paste0(calc," ",options," ",file," ",library_file))
                 }
+                
                 if(!is.null(parse_output_to)){
+                        
                         system(paste0(calc," ",options," ",file," ",library_file," >",parse_output_to))
                 }
         }
+        
+        
         if(!is.null(path)){
+                
                 if(is.null(parse_output_to)){
+                        
                         system(paste0(path,calc," ",options," ",file," ",library_file))
-                }        
+                }    
+                
                 if(!is.null(parse_output_to)){
+                        
                         system(paste0(path,calc," ",options," ",file," ",library_file," >",parse_output_to))
                 }
         }
 }
+
+
+
+
+
+
