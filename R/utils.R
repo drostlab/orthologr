57211024e7603c77b1e622e1928028722070d129
HajkD <hajkdrost@googlemail.com>
2015-02-22 12:17:47 +0100

    adding new utility function cds2aa()

--- R/utils.R
+++ R/utils.R
@@ -91,6 +91,44 @@ is.alignment_search_tool <- function(tool = NULL){
         return(is.element(tool, provided))
 }
 
+#' @title Translate CDS file to Amino Acids file
+#' @import data.table
+cds2aa <- function(dt){
+        
+        if(!is.data.table(dt))
+                stop("Your CDS file was not corretly transformed into a data.table object.")
+        
+        
+        # When using data.tables within packaes, always make sure
+        # 'data.table' is included in the DESCRIPTION file as 'Imports' AND
+        # in the NAMESPACE file with 'imports(data.table)' -> @import when using roxygen2
+        # http://stackoverflow.com/questions/10527072/using-data-table-package-inside-my-own-package
+        # https://github.com/hadley/dplyr/issues/548
+        
+        # omit empty sequences
+        dt <- dt[ ,.SD[sapply(seqs,function(x){return(! (is.na(x) || x=="") )})]]
+        
+        # omit sequences taht are not multiples of 3
+        dt <- dt[ ,.SD[sapply(seqs,function(x){return(nchar(x)%%3==0)})]]
+        
+        # omit sequences consisting of others than ACGT
+        dt <- dt[ ,.SD[sapply(seqs,is.dnaSequence)]]
+        
+        # translate cds to protein sequences
+        tryCatch({
+                
+                dt[ , aa := transl(seqs), by = geneids]
+                
+        }, error = function(e) {stop("The input coding sequences could not be translated properly to amino acid sequences.",
+                                     ,"\n"," Please check whether ",file, " stores valid coding sequences.")}
+        )
+        
+        return(dt)
+}
+        
+
+
+
 
 #         
 #         is. ... <- function(tool=NULL){
