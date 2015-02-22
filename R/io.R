03e7ead437afba14b0fe6f0004f5466afb47f7dc
HajkD <hajkdrost@googlemail.com>
2015-02-22 14:24:51 +0100

    adding faster implementations of read.cds(), read.proteome(), and read.genome() that are now based on the input functions provided by the Biostrings package

--- R/io.R
+++ R/io.R
@@ -1,13 +1,13 @@
 #' @title Read the genome of a given organism
 #' @description This function reads an organism specific genome stored in a defined file format.
 #' @param file a character string specifying the path to the file storing the genome.
-#' @param format a character string specifying the file format used to store the genome, e.g. "fasta", "gbk".
-#' @param ... additional arguments that are used by the seqinr::read.fasta() function.
+#' @param format a character string specifying the file format used to store the genome, e.g. "fasta", "fastq".
+#' @param ... additional arguments that are used by the \code{\link[Biostrings]{readDNAStringSet}} function.
 #' @author Hajk-Georg Drost and Sarah Scharfenberg
 #' @details The \code{read.genome} function takes a string specifying the path to the genome file
 #' of interest as first argument.
 #'
-#' It is possible to read in different genome file standards such as \emph{fasta} or \emph{genebank}.
+#' It is possible to read in different genome file standards such as \emph{fasta} or \emph{fastq}.
 #' Genomes stored in fasta files can be downloaded from http://ensemblgenomes.org/info/genomes.
 #'
 #' @examples \dontrun{
@@ -21,24 +21,25 @@
 #' @export
 
 read.genome <- function(file, format, ...){
-        if(!is.element(format,c("fasta","gbk")))
+        
+        if(!is.element(format,c("fasta","fastq")))
                 stop("Please choose a file format that is supported by this function.")
         
-        geneids <- NULL
+        geneids <- seqs <- NULL
         
         if(format == "fasta"){
-                genome <- vector(mode = "list")
                 
-                tryCatch(
-                        {
-                                 genome <- seqinr::read.fasta(file, seqtype = "DNA", ...)
-                                 genome_names <- unlist(lapply(names(genome), function(x){return(strsplit(x, "\t")[[1]][1])}))
-                                 genome.dt <- data.table::data.table(geneids = genome_names,
-                                                                     seqs = unlist(lapply(genome, seqinr::c2s)))
-                                 data.table::setkey(genome.dt,geneids)
-                                 
-                        }, error = function(e){ stop("File ",file, " could not be read properly. \n",
-                        "Please make sure that ",file," contains only DNA sequences and is in ",format," format.")}
+                tryCatch({
+                        
+                        genome <- Biostrings::readDNAStringSet(filepath = file, format = format, ...)
+                        genome_names <- as.vector(unlist(sapply(genome@ranges@NAMES, function(x){return(strsplit(x, " ")[[1]][1])})))
+                        genome.dt <- data.table::data.table(geneids = genome_names,
+                                                            seqs = toupper(as.character(genome)))
+                        
+                        data.table::setkey(genome.dt,geneids)
+                        
+                }, error = function(e){ stop(paste0("File ",file, " could not be read properly. \n",
+                                                    "Please make sure that ",file," contains only DNA sequences and is in ",format," format."))}
                 )
         }
         
@@ -49,13 +50,13 @@ read.genome <- function(file, format, ...){
 #' @title Read the proteome of a given organism
 #' @description This function reads an organism specific proteome stored in a defined file format.
 #' @param file a character string specifying the path to the file storing the proteome.
-#' @param format a character string specifying the file format used to store the proteome, e.g. "fasta", "gbk".
-#' @param ... additional arguments that are used by the seqinr::read.fasta() function.
-#' @author Hajk-Georg Drost and Sarah Scharfenberg
+#' @param format a character string specifying the file format used to store the proteome, e.g. "fasta", "fastq".
+#' @param ... additional arguments that are used by the \code{\link[Biostrings]{readAAStringSet}} function.
+#' @author Hajk-Georg Drost
 #' @details The \code{read.proteome} function takes a string specifying the path to the proteome file
 #' of interest as first argument.
 #'
-#' It is possible to read in different proteome file standards such as \emph{fasta} or \emph{genebank}.
+#' It is possible to read in different proteome file standards such as \emph{fasta} or \emph{fastq}.
 #'
 #' Proteomes stored in fasta files can be downloaded from http://www.ebi.ac.uk/reference_proteomes.
 #'
@@ -70,26 +71,25 @@ read.genome <- function(file, format, ...){
 #' @export
 
 read.proteome <- function(file, format, ...){
-        if(!is.element(format,c("fasta","gbk")))
+        
+        if(!is.element(format,c("fasta","fastq")))
                 stop("Please choose a file format that is supported by this function.")
         
-        geneids <- NULL 
+        geneids <- seqs <- NULL 
         
-        if(format == "fasta"){
-                proteome <- vector(mode = "list")
                 
-                tryCatch(
-                         {
-                                 proteome <- seqinr::read.fasta(file, seqtype = "AA", ...)
-                                 proteome_names <- unlist(lapply(names(proteome), function(x){return(strsplit(x, "\t")[[1]][1])}))
-                                 proteome.dt <- data.table::data.table(geneids = proteome_names,
-                                                                       seqs = unlist(lapply(proteome, seqinr::c2s)))
-                                 data.table::setkey(proteome.dt,geneids)
-                                 
-                         }, error = function(e){ stop("File ",file, " could not be read properly. \n",
-                                                             "Please make sure that ",file," contains only amino acid sequences and is in ",format," format.")}
+                tryCatch({
+                        
+                        proteome <- Biostrings::readAAStringSet(filepath = file, format = format, ...)
+                        proteome_names <- as.vector(unlist(sapply(proteome@ranges@NAMES, function(x){return(strsplit(x, " ")[[1]][1])})))
+                        proteome.dt <- data.table::data.table(geneids = proteome_names,
+                                                              seqs = toupper(as.character(proteome)))
+                        
+                        data.table::setkey(proteome.dt, geneids)
+                        
+                }, error = function(e){ stop("File ",file, " could not be read properly.", "\n",
+                                                    "Please make sure that ",file," contains only amino acid sequences and is in ",format," format.")}
                 )
-        }
         
         return(proteome.dt)
 }
@@ -98,13 +98,13 @@ read.proteome <- function(file, format, ...){
 #' @title Read the CDS of a given organism
 #' @description This function reads an organism specific CDS stored in a defined file format.
 #' @param file a character string specifying the path to the file storing the CDS.
-#' @param format a character string specifying the file format used to store the CDS, e.g. "fasta", "gbk".
-#' @param ... additional arguments that are used by the seqinr::read.fasta() function.
-#' @author Hajk-Georg Drost and Sarah Scharfenberg
+#' @param format a character string specifying the file format used to store the CDS, e.g. "fasta", "fatsq".
+#' @param ... additional arguments that are used by the \code{\link[Biostrings]{readDNAStringSet}} function.
+#' @author Hajk-Georg Drost
 #' @details The \code{read.cds} function takes a string specifying the path to the cds file
 #' of interest as first argument.
 #'
-#' It is possible to read in different proteome file standards such as \emph{fasta} or \emph{genebank}.
+#' It is possible to read in different proteome file standards such as \emph{fasta} or \emph{fastq}.
 #'
 #' CDS stored in fasta files can be downloaded from http://www.ensembl.org/info/data/ftp/index.html.
 #'
@@ -118,32 +118,50 @@ read.proteome <- function(file, format, ...){
 #' sequence as string in the second column.
 #' @export
 
-read.cds <- function(file, format, ...){
-        if(!is.element(format,c("fasta","gbk")))
+read.cds <- function(file, format, delete_corrupt = TRUE, ...){
+        
+        if(!is.element(format,c("fasta","fastq")))
                 stop("Please choose a file format that is supported by this function.")
         
-        geneids <- NULL
         
-        if(format == "fasta"){
-                cds <- vector(mode = "list")
-                
-                tryCatch(
-                        {
-                                cds <- seqinr::read.fasta(file, seqtype = "DNA", ...)
-                                cds_names <- unlist(lapply(names(cds), function(x){return(strsplit(x, "\t")[[1]][1])}))
-                                cds.dt <- data.table::data.table(geneids = cds_names ,
-                                                 seqs = unlist(lapply(cds, seqinr::c2s)))
-                                data.table::setkey(cds.dt,geneids)
-                
-                        }, error = function(e){ stop("File ",file, " could not be read properly. \n",
-                                                            "Please make sure that ",file," contains only CDS sequences and is in ",format," format.")}
+        geneids <- seqs <- NULL
+        
+                tryCatch({
+                        
+                        cds_file <- Biostrings::readDNAStringSet(filepath = file, format = format, ...)
+                        
+                        cds_names <- as.vector(unlist(sapply(cds_file@ranges@NAMES, function(x){return(strsplit(x, " ")[[1]][1])})))
+                        
+                        cds.dt <- data.table::data.table(geneids = cds_names ,
+                                                         seqs = tolower(as.character(cds_file)))
+                        
+                        
+                        data.table::setkey(cds.dt, geneids)
+                        
+                        mod3 <- function(x) { return((nchar(x) %% 3) == 0) }
+                        
+                        all_triplets <- cds.dt[ , mod3(seqs)]
+                        
+                                           
+                }, error = function(e){ stop("File ",file, " could not be read properly.","\n",
+                                                    "Please make sure that ",file," contains only CDS sequences and is in ",format," format.")}
                 )
+        
+        if(!all(all_triplets)){
+                
+                if(delete_corrupt)
+                        return(cds.dt[-which(!all_triplets) , list(geneids, seqs)])
+                
+                if(!delete_corrupt)
+                        return(cds.dt)
+                
+        } else {
+                
+                return(cds.dt)
         }
         
-        return(cds.dt)
+        
 }
-
-
 
 #' @title Translate DNA to Amino Acids
 #' @description This function takes CDS sequence as string as input an returns the
