0a12808eda6aa2b2f0505326abfe63d38d0665ab
HajkD <hajkdrost@googlemail.com>
2015-02-22 15:36:31 +0100

    adding new function write.proteome()

--- R/io.R
+++ R/io.R
@@ -163,6 +163,44 @@ read.cds <- function(file, format, delete_corrupt = TRUE, ...){
         
 }
 
+
+#' @title Save a proteome in fasta format
+#' @description This function takes a proteome (in data.table notation) as returned by 
+#' \code{\link{read.proteome}} and saves it to your hard drive.
+#' @param proteome a data.table object storing amino acid sequences in the first column
+#' and gene ids in the second column. See \code{\link{read.proteome}} for details.
+#' @param file.name a character string specifying the name of the fasta output file.
+#' @param nbchar number of characters per line.
+#' @param mode to open the output file, use "w" to write into a new file, use "a" to append at the end of an already existing file.
+#' @param as.string when set to TRUE sequences are in the form of strings instead of vectors of single characters.
+#' @author Hajk-Georg Drost
+#' @examples
+#' 
+#' \dontrun{
+#' 
+#' # read an example proteome
+#' Ath.proteome <- read.proteome(system.file('seqs/ortho_thal_aa.fasta', package = 'orthologr'), format = "fasta")
+#' 
+#' # use the write.proteome function to store it on your hard drive
+#' write.proteome(Ath.proteome, "test_proteome.fasta")
+#' 
+#' }
+#' 
+#' @export
+
+write.proteome <- function(proteome, file.name, nbchar = 80, open = "w", as.string = TRUE){
+        
+        seqinr::write.fasta( sequences = as.list(proteome[ , seqs]),
+                             names     = proteome[ , geneids],
+                             nbchar    = nbchar, 
+                             open      = open,
+                             file.out  = file.name,
+                             as.string = as.string)
+        
+        
+}
+
+
 #' @title Translate DNA to Amino Acids
 #' @description This function takes CDS sequence as string as input an returns the
 #' corresponding amino acid sequence as string. 
