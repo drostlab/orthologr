6e54f7016c81918eb9cce67b9630aa7b21d58e5d
HajkD <hajkdrost@googlemail.com>
2018-11-07 16:07:24 +0000

    update ortholog selection function

--- R/select_orthologs.R
+++ R/select_orthologs.R
@@ -16,8 +16,7 @@ select_orthologs <-
                  annotation_file_query,
                  annotation_file_subject,
                  collapse_by = "gene_locus",
-                 format = c("gtf","gtf")) {
-                
+                 format = c("gtf", "gtf")) {
                 if (!is.element(collapse_by, c("gene_locus", "splice_variant")))
                         stop(
                                 "Please specify a collapse method provided by this function: collapse_by = 'gene_locus' or collapse_by = 'splice_variant'.",
@@ -25,10 +24,16 @@ select_orthologs <-
                         )
                 
                 if (!all(is.element(format, c("gtf", "gff"))))
-                        stop("Please provide a vector of length 2 that contains available formats: either 'gtf' or 'gff'.", call. = FALSE)
+                        stop(
+                                "Please provide a vector of length 2 that contains available formats: either 'gtf' or 'gff'.",
+                                call. = FALSE
+                        )
                 
                 if (length(format) != 2)
-                        stop("Please provide a vector of length 2 to specify the query and subject annotation file formats. E.g. format = c('gtf', 'gtf').", call. = FALSE)
+                        stop(
+                                "Please provide a vector of length 2 to specify the query and subject annotation file formats. E.g. format = c('gtf', 'gtf').",
+                                call. = FALSE
+                        )
                 
                 if (!file.exists(annotation_file_query))
                         stop(
@@ -46,9 +51,17 @@ select_orthologs <-
                                 call. = FALSE
                         )
                 
-                message("Start ortholog selection process by ",collapse_by, " ...")
+                message("Start ortholog selection process by ",
+                        collapse_by,
+                        " ...")
                 
-                message("Import query annotation file '", annotation_file_query, "' using format ",format[1], " ...")
+                message(
+                        "Import query annotation file '",
+                        annotation_file_query,
+                        "' using format ",
+                        format[1],
+                        " ..."
+                )
                 
                 # import annotation for query
                 imported_annotation_qry <-
@@ -58,8 +71,10 @@ select_orthologs <-
                 if (format[1] == "gff") {
                         Name <- NULL
                         extracted_features_qry <-
-                                dplyr::do(dplyr::group_by(imported_annotation_qry, Name),
-                                          extract_features(. , format = format[1]))
+                                dplyr::do(
+                                        dplyr::group_by(imported_annotation_qry, Name),
+                                        extract_features(. , format = format[1])
+                                )
                         colnames(extracted_features_qry)[1] <-
                                 "query_Name"
                 }
@@ -67,13 +82,21 @@ select_orthologs <-
                 if (format[1] == "gtf") {
                         gene_id <- NULL
                         extracted_features_qry <-
-                                dplyr::do(dplyr::group_by(imported_annotation_qry, gene_id),
-                                          extract_features(. , format = format[1]))
+                                dplyr::do(
+                                        dplyr::group_by(imported_annotation_qry, gene_id),
+                                        extract_features(. , format = format[1])
+                                )
                         colnames(extracted_features_qry)[1] <-
                                 "query_Name"
                 }
                 
-                message("Import subject annotation file '", annotation_file_subject, "' using format ",format[2], " ...")
+                message(
+                        "Import subject annotation file '",
+                        annotation_file_subject,
+                        "' using format ",
+                        format[2],
+                        " ..."
+                )
                 # import annotation for subjecy
                 imported_annotation_sbj <-
                         tibble::as_tibble(rtracklayer::import.gff(annotation_file_subject, format = format[2]))
@@ -83,8 +106,10 @@ select_orthologs <-
                 if (format[2] == "gff") {
                         Name <- NULL
                         extracted_features_sbj <-
-                                dplyr::do(dplyr::group_by(imported_annotation_sbj, Name),
-                                          extract_features(. , format = format[2]))
+                                dplyr::do(
+                                        dplyr::group_by(imported_annotation_sbj, Name),
+                                        extract_features(. , format = format[2])
+                                )
                         colnames(extracted_features_sbj)[1] <-
                                 "subject_Name"
                         colnames(extracted_features_sbj)[3] <-
@@ -94,8 +119,10 @@ select_orthologs <-
                 if (format[2] == "gtf") {
                         gene_id <- NULL
                         extracted_features_sbj <-
-                                dplyr::do(dplyr::group_by(imported_annotation_sbj, gene_id),
-                                          extract_features(. , format = format[2]))
+                                dplyr::do(
+                                        dplyr::group_by(imported_annotation_sbj, gene_id),
+                                        extract_features(. , format = format[2])
+                                )
                         colnames(extracted_features_sbj)[1] <-
                                 "subject_gene_id"
                         colnames(extracted_features_sbj)[3] <-
@@ -105,27 +132,66 @@ select_orthologs <-
                 message("Join dnds and annotation tables ...")
                 # join dnds tbl with query annotation
                 joined_dnds_annotation_tbl_qry <-
-                        dplyr::inner_join(dplyr::ungroup(dnds_tbl), dplyr::ungroup(extracted_features_qry), by = "query_id")
+                        dplyr::inner_join(dnds_tbl,
+                                          extracted_features_qry,
+                                          by = "query_id")
+                
+                names(joined_dnds_annotation_tbl_qry)[length(joined_dnds_annotation_tbl_qry)] <-
+                        "query_gene_locus_id"
+                
+                print(joined_dnds_annotation_tbl_qry)
                 
                 # join dnds tbl with subject annotation
                 joined_dnds_annotation_tbl_sbj <-
-                        dplyr::inner_join(dplyr::ungroup(dnds_tbl), dplyr::ungroup(extracted_features_sbj), by = "subject_id")
+                        dplyr::inner_join(dnds_tbl,
+                                          extracted_features_sbj,
+                                          by = "subject_id")
+                
+                names(joined_dnds_annotation_tbl_sbj)[length(joined_dnds_annotation_tbl_sbj)] <-
+                        "subject_gene_locus_id"
+                
+                joined_dnds_annotation_tbl_sbj <-
+                        dplyr::select(
+                                joined_dnds_annotation_tbl_sbj,
+                                "query_id",
+                                "subject_id",
+                                "subject_gene_id",
+                                "subject_gene_locus_id"
+                        )
+                
+                print(joined_dnds_annotation_tbl_sbj)
+                
                 # join query and subject tables
                 final_join_dnds_annotation_tbl <-
-                        dplyr::inner_join(dplyr::ungroup(joined_dnds_annotation_tbl_qry),
-                                          dplyr::ungroup(joined_dnds_annotation_tbl_sbj),
-                                          by = c("query_id", "subject_id"))
+                        dplyr::inner_join(
+                                joined_dnds_annotation_tbl_qry,
+                                joined_dnds_annotation_tbl_sbj,
+                                by = c("query_id", "subject_id")
+                        )
                 
                 if (collapse_by == "gene_locus") {
-                        gene_locus_id <- NULL
-                        res <-
+                        query_gene_locus_id <- subject_gene_locus_id <- NULL
+                        res_qry <-
                                 dplyr::do(
                                         dplyr::group_by(
                                                 final_join_dnds_annotation_tbl,
-                                                gene_locus_id
+                                                query_gene_locus_id
                                         ),
                                         filter_best_hits(.)
                                 )
+                        
+                        res_sbj <-
+                                dplyr::do(
+                                        dplyr::group_by(
+                                                final_join_dnds_annotation_tbl,
+                                                subject_gene_locus_id
+                                        ),
+                                        filter_best_hits(.)
+                                )
+                        
+                        res <- dplyr::inner_join(res_qry,
+                                                 res_sbj,
+                                                 by = c("query_id", "subject_id"))
                 }
                 
                 if (collapse_by == "splice_variant") {
@@ -135,8 +201,3 @@ select_orthologs <-
                 message("... orthology inference finished successfully!")
                 return(res)
         }
-
-
-
-
-
