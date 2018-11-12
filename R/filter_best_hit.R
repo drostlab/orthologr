47fa9d2771ecc6cb5ceba194c428e037cf131489
HajkD <hajkdrost@googlemail.com>
2018-11-12 18:10:01 +0000

    retrieve splice variant that generates longest alignment in case e-values are identical

--- R/filter_best_hit.R
+++ R/filter_best_hit.R
@@ -1,6 +1,16 @@
 #' @title Helper function to select best BLAST hit based on min evalue
 filter_best_hits <- function(x) {
-        evalue <- NULL
-        res <- dplyr::filter(x, evalue == min(evalue))
+        
+        min_val <- min(x$evalue)
+        evalue <- alig_length <- NULL
+        res <- dplyr::filter(x, evalue == min_val)
+        if (nrow(res) > 1) {
+                max_len <- max(res$alig_length)
+                res <- dplyr::filter(res, alig_length == max_len)
+        }
+        
+        if (nrow(res) > 1) 
+                res <- dplyr::slice(res, 1)
+        
         return(res)
 }
