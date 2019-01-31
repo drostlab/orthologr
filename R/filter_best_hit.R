b11902d8bf2a95189a8073b36a042c874463eecf
HajkD <hajkdrost@googlemail.com>
2019-01-31 12:30:42 +0000

    add docu

--- R/filter_best_hit.R
+++ R/filter_best_hit.R
@@ -1,4 +1,8 @@
 #' @title Helper function to select best BLAST hit based on minimum evalue
+#' @details Extracts the best hit based on lowest e-value and longest splice variant 
+#' when e-values are equal
+#' @param x a tibble storing gene locus ids, splice variant ids, and blast output for filtering
+#' @author Hajk-Georg Drost
 filter_best_hits <- function(x) {
         
         min_val <- min(x$evalue)
