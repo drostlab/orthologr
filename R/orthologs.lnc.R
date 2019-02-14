8d556c2f754a19e0bfb88b03624b1976d36543e2
HajkD <hgd23@cam.ac.uk>
2019-02-14 19:29:49 +0000

    add orthology inference for lncRNAs

--- R/orthologs.lnc.R
+++ R/orthologs.lnc.R
@@ -4,28 +4,16 @@
 #' based on selected orthology inference programs.
 #' @param query_file a character string specifying the path to the sequence file of interest (query organism).
 #' @param subject_file a character string specifying the paths to the sequence files of interest (subject organisms).
-#' Different orthology inference methods can detect orthologs using multiple subject organisms, e.g. "OrthoMCL", and "PO" (ProteinOrtho).
-#' @param outgroup_file a character string specifying the paths to the sequence files of interest (outgroup organisms).
-#' This argument is only used by \code{InParanoid}.
 #' @param eval a numeric value specifying the E-Value cutoff for BLAST hit detection.
-#' @param seq_type a character string specifying the sequence type stored in the input file.
-#' Options are are: "cds", "protein", or "dna". In case of "cds", sequence are translated to protein sequences,
-#' in case of "dna", cds prediction is performed on the corresponding sequences which subsequently are
-#' translated to protein sequences. Default is \code{seq_type} = "protein".
-#' @param format a character string specifying the file format of the sequence file, e.g. "fasta", "gbk". Default is "fasta".
 #' @param ortho_detection a character string specifying the orthology inference method that shall be performed
-#' to detect orthologous genes. Default is \code{ortho_detection} = "RBH" (BLAST reciprocal best hit).
-#' Further methods are: "RBH" (BLAST reciprocal best hit), "PO" (ProteinOrtho), and "OrthoMCL.
-#' @param delete_corrupt_cds a logical value indicating whether sequences with corrupt base triplets should be removed from the input \code{file}. This is the case when the length of coding sequences cannot be divided by 3 and thus the coding sequence contains at least one corrupt base triplet. 
-#' @param cdd.path path to the cdd database folder (specify when using \code{ortho_detection} = \code{"DELTA"}).
+#' to detect orthologous genes. Options are:
+#' \itemize{
+#' \item \code{ortho_detection = "RBH"} (BLAST reciprocal best hit) (Default)
+#' \item \code{ortho_detection = "BH"} (BLAST best hit)
+#' }
 #' @param path a character string specifying the path to the corresponding orthology inference tool.
 #' For "BH" and "RBH": path to BLAST, "PO": path to ProteinOrtho 5.07, "OrthoMCL": path to OrthoMCL.
-#' @param add_params a character string specifying additional parameters that shall be handed to the orthology inference method (tool).
-#' Default is \code{add_params} = \code{NULL}.
 #' @param comp_cores a numeric value specifying the number of cores to be used for multicore computations.
-#' @param quiet a logical value specifying whether a successful interface call shall be printed out.
-#' @param clean_folders a boolean value spefiying whether all internall folders storing the output of used programs
-#' shall be removed. Default is \code{clean_folders} = \code{FALSE}.
 #' @details 
 #' This function takes sequence files of a query organism and a subject organism and performs orthology inference
 #' using a defined orthology inference method to dectect orthologous genes.
@@ -37,7 +25,6 @@
 #' \itemize{
 #'   \item BLAST best hit (BH) 
 #'   \item BLAST reciprocal best hit (RBH)
-#'   \item DELTA-BLAST reciprocal best hit (DELTA)
 #' }
 #' 
 #' 
@@ -52,31 +39,25 @@
 #'
 #' @examples \dontrun{
 #' 
-#' 
-#' ### BLAST Best Hit
-#' 
-#' # perform orthology inference using BLAST best hit
-#' # and fasta sequence files storing protein sequences
-#' orthologs(query_file      = system.file('seqs/ortho_thal_aa.fasta', package = 'orthologr'),
-#'           subject_file   = system.file('seqs/ortho_lyra_aa.fasta', package = 'orthologr'),
-#'           seq_type        = "protein", 
-#'           ortho_detection = "BH")
-#' 
-#' 
 #' ### BLAST Reciprocal Best Hit
-#' 
 #' # perform orthology inference using BLAST reciprocal best hit
 #' # and fasta sequence files storing protein sequences
-#' orthologs(query_file      = system.file('seqs/ortho_thal_aa.fasta', package = 'orthologr'),
-#'           subject_file   = system.file('seqs/ortho_lyra_aa.fasta', package = 'orthologr'),
-#'           seq_type        = "protein", 
+#' orthologs.lnc(query_file      = system.file('seqs/ortho_thal_cds.fasta', package = 'orthologr'),
+#'           subject_file   = system.file('seqs/ortho_lyra_cds.fasta', package = 'orthologr'),
 #'           ortho_detection = "RBH")
 #'           
 #'           
+#' ### BLAST Best Hit
+#' # perform orthology inference using BLAST best hit
+#' # and fasta sequence files storing protein sequences
+#' orthologs.lnc(query_file = system.file('seqs/ortho_thal_cds.fasta', package = 'orthologr'),
+#'           subject_file = system.file('seqs/ortho_lyra_cds.fasta', package = 'orthologr'),
+#'           ortho_detection = "BH")
+#' 
+#'
 #' # multicore version          
-#' orthologs(query_file      = system.file('seqs/ortho_thal_aa.fasta', package = 'orthologr'),
-#'           subject_file   = system.file('seqs/ortho_lyra_aa.fasta', package = 'orthologr'),
-#'           seq_type        = "protein", 
+#' orthologs.lnc(query_file      = system.file('seqs/ortho_thal_cds.fasta', package = 'orthologr'),
+#'           subject_file   = system.file('seqs/ortho_lyra_cds.fasta', package = 'orthologr'),
 #'           ortho_detection = "RBH", 
 #'           comp_cores      = 2)          
 #'           
@@ -105,30 +86,28 @@ orthologs.lnc <- function(query_file,
                 if (length(subject_file) > 1)
                         stop("The BLAST best hit method is only defined for pairwise comparisons.", call. = FALSE)
                 
-                metablastr::blast_best(
+                ortho_tbl <- metablastr::blast_best_hit(
                         query      = query_file,
                         subject =  subject_file,
+                        search_type = "nucleotide_to_nucleotide",
                         cores = comp_cores,
                         task = "blastn",
                         evalue = eval, 
                         output.path = output.path,
                         max.target.seqs = max.target.seqs 
                 )
-                        
-                if (clean_folders)
-                        clean_all_folders(file.path(tempdir(),"_blast_db"))
                 
         }
-        
-        
-        if(ortho_detection == "RBH"){
+
+        if (ortho_detection == "RBH") {
                 
                 if (length(subject_file) > 1)
-                        stop("The BLAST best reciprocal hit method is only defined for pairwise comparisons.")
+                        stop("The BLAST best reciprocal hit method is only defined for pairwise comparisons.", call. = FALSE)
                 
-                metablastr::blast_rec(
+                ortho_tbl <- metablastr::blast_best_reciprocal_hit(
                         query      = query_file,
                         subject =  subject_file,
+                        search_type = "nucleotide_to_nucleotide",
                         cores = comp_cores,
                         task = "blastn",
                         evalue = eval, 
@@ -136,89 +115,7 @@ orthologs.lnc <- function(query_file,
                         max.target.seqs = max.target.seqs 
                 )
 
-                if(clean_folders)
-                        clean_all_folders(file.path(tempdir(),"_blast_db"))
                 
-        }
-        
-        
-        if(ortho_detection == "DELTA"){
-                
-                pars <- paste0(add_params," -evalue ",eval," -num_threads ",comp_cores,
-                               " -best_hit_score_edge 0.05 -best_hit_overhang 0.25 -max_target_seqs 1")
-                
-                ortho_tbl_A <- data.table::copy(
-                        
-                        advanced_blast( query_file      = query_file, 
-                                        subject_file    = subject_file,
-                                        blast_algorithm = "deltablast",
-                                        db_path         = cdd.path,
-                                        path            = path, 
-                                        blast_params    = pars, 
-                                        seq_type        = seq_type,  
-                                        format          = format )
-                        
-                )
-                
-                ortho_tbl_B <- data.table::copy(
-                        
-                        advanced_blast( query_file      = subject_file, 
-                                        subject_file    = query_file,
-                                        blast_algorithm = "deltablast",
-                                        db_path         = cdd.path,
-                                        path            = path, 
-                                        blast_params    = pars, 
-                                        seq_type        = seq_type,  
-                                        format          = format )
-                        
-                )
-                
-                data.table::setnames(ortho_tbl_B, old = c("query_id","subject_id"), new = c("subject_id","query_id"))
-                
-                tryCatch({       
-                        
-                        delta_rec_tbl <- dplyr::semi_join(dplyr::tbl_dt(ortho_tbl_A), dplyr::tbl_dt(ortho_tbl_B),
-                                                          by = c("query_id","subject_id"))
-                        
-                        if(detailed_output){
-                                
-                                return ( delta_rec_tbl )
-                        }
-                        
-                        if(!detailed_output){
-                                
-                                return ( delta_rec_tbl[ ,list(query_id,subject_id,evalue)] )
-                        }
-                        
-                        
-                }, error = function(e){ stop("The BLAST tables resulting from ",query_file, " and ",
-                                             subject_file," could not be joined properly to select only the reciprocal best hits.")}
-                )
-                
-                if(clean_folders)
-                        clean_all_folders(file.path(tempdir(),"_blast_db"))
-                
-        }
-        
-        if(ortho_detection == "Orthofinder2"){
-                
-                message("This option is currently under development and will be available soon.")
-                # ortho_tbl <- data.table::copy(
-                #         
-                #         ProteinOrtho( query_file    = query_file,
-                #                       subject_file = subject_file, 
-                #                       po_path       = path, 
-                #                       eval          = eval, 
-                #                       comp_cores    = comp_cores,
-                #                       po_params     = add_params, 
-                #                       seq_type      = seq_type, 
-                #                       format        = format )
-                #         
-                #         )
-                # 
-                # 
-                # if(clean_folders)
-                #         clean_all_folders(file.path(tempdir(),"_ProteinOrtho"))
                 
         }
         
