9575877adee58bf1175b7e1ae0ce2c83e543d4ea
HajkD <hgd23@cam.ac.uk>
2019-03-18 18:39:36 +0000

    add global variables

--- R/retrieve_longest_isoforms.R
+++ R/retrieve_longest_isoforms.R
@@ -29,12 +29,16 @@
 #' @export
 retrieve_longest_isoforms <- function(proteome_file, annotation_file, new_file, annotation_format = "gtf") {
         
-        if (!fs::file_exists(proteome_file))
-                stop("File ", proteome_file, " seems to not exist.", call. = FALSE)
-        if (!fs::file_exists(annotation_file))
-                stop("File ", annotation_file, " seems to not exist.", call. = FALSE)
+        if (!file.exists(proteome_file))
+                stop("File ", proteome_file, " seems not to exist.", call. = FALSE)
+        
+        if (!file.exists(annotation_file))
+                stop("File ", annotation_file, " seems not to exist.", call. = FALSE)
+        
         if (!is.element(annotation_format, c("gtf", "gff")))
                 stop("Please specify a valid annotation format. Options are: annotation_file = 'gtf' or annotation_file = 'gff'.", call. = FALSE)
+        
+        '.' <- NULL
         
         message("Extracting longest isoform from ", basename(proteome_file), " ...")
         message("Importing proteome file ", basename(proteome_file), " ...")
@@ -85,6 +89,7 @@ retrieve_longest_isoforms <- function(proteome_file, annotation_file, new_file, 
                 gff_import <- tibble::as_tibble(rtracklayer::import(annotation_file))
                 gene_biotype <- gene <- transcript_id <- type <- width <- NULL
                 gff_import <- dplyr::filter(gff_import, type == "CDS")
+                protein_id <- NULL
                 gff_import <- dplyr::select(gff_import, gene, protein_id)
                 #print(gff_import)
                 # join transcript_ids from annotation file with peptide sequence file
@@ -97,7 +102,7 @@ retrieve_longest_isoforms <- function(proteome_file, annotation_file, new_file, 
                 }
                 message("Retrieving longest isoforms ...")
                 res <- dplyr::do(dplyr::group_by(pep_file_tibble_joined, gene), extract_longest_transcript(.))
-                matched_ids <- na.omit(match(res$protein_id, pep_file@ranges@NAMES))
+                matched_ids <- stats::na.omit(match(res$protein_id, pep_file@ranges@NAMES))
                 
                 if (length(matched_ids) == 0)
                         stop("Gene_ID or Peptide_IDs between the gtf file and the headers of the fasta file did not match!",
