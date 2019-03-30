ef050f4209879e8ca3ebb5a830998382b96fb40c
HajkD <hajkdrost@googlemail.com>
2019-03-30 18:40:46 +0000

    add helper function to test if orthofinder2 is installed

---
+++ R/is_installed_orthofinder.R
@@ -0,0 +1,15 @@
+is_installed_orthofinder <- function() {
+        # test if a valid BLAST version is installed
+        tryCatch({
+                sys_out <-
+                        system("orthofinder -version", intern = TRUE)
+        }, error = function(e)
+                stop(
+                        "It seems like you don't have OrthoFinder2 installed locally on your machine or the PATH variable to the OrthoFinder2 program is not set correctly.",
+                        call. = FALSE
+                ))
+        
+        if (any(stringr::str_detect(sys_out, "OrthoFinder")))
+                return(TRUE)
+        
+}
