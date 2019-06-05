5c6b34c28f07cdf1e96b48e2a6ca10d4c015ea38
HajkD <hgd23@cam.ac.uk>
2019-06-05 19:12:26 +0100

    update example of orthofinder2

--- R/orthofinder2.R
+++ R/orthofinder2.R
@@ -9,6 +9,14 @@
 #'           "Capsella rubella", "Solanum lycopersicum")
 #' # download proteome files for all species          
 #' biomartr::getProteomeSet(db = "refseq", organisms = orgs, path = "of_proteomes")
+#' # download annotation files for all species          
+#' biomartr::getGFFSet(db = "refseq", organisms = orgs, path = "of_gff")
+#' # select longest splice variant per gene locus
+#' dir.create("of_proteomes_longest_sv")
+#' retrieve_longest_isoforms(proteome_file = "of_proteomes/Alyrata.faa", 
+#'                           annotation_file = "of_gff/Alyrata.gff",
+#'                           annotation_format = "gff", 
+#'                           new_file = "of_proteomes_longest_sv/Alyrata.faa")
 #' # run orthofinder2 to infer ortho groups for the specified species
 #' orthofinder2(proteome_folder = "of_proteomes", comp_cores = 4)
 #' } 
@@ -17,25 +25,28 @@ orthofinder2 <- function(proteome_folder, comp_cores = 1) {
         
         is_installed_orthofinder()
         
+        ### add here a test that input sequences are amino acid sequences
+        
         if (!file.exists(proteome_folder))
                 stop("Please provide a valid path to the proteome folder. Your specified path '", proteome_folder, "' seems not to exist.", call. = FALSE)
         
         files <- list.files(proteome_folder)
         
-        if (stringr::str_detect(files, "documentation")){
+        if (stringr::str_detect(files, "documentation")) {
                 message("Your species folder '", proteome_folder, " still contains the 'documentation' folder which will be moved from ", proteome_folder, " to ", getwd(), " to enable the Orthofinder2 search.")
                 file.rename(file.path(proteome_folder, "documentation"), file.path(getwd(), "documentation"))
                 unlink(file.path(proteome_folder, "documentation"), recursive = TRUE, force = TRUE)
         }
         
-        # determine the number of cores on a multicore machine
+        # determine the number of cores on a multicore machineYes
         cores <- parallel::detectCores()
         
         # in case one tries to use more cores than are available
         if (comp_cores > cores)
-                stop("You chose more cores than are available on your machine.")
+                stop("You chose more cores than are available on your machine.", call. = FALSE)
         
         message("Running ", system("orthofinder", intern = TRUE)[1], " using ", comp_cores, " cores ...")
+        message("Do your input fasta files store unique sequences per gene locus, e.g. the longest splice variant? If not, then have a look at ?orthologr::retrieve_longest_isoforms().")
         
         # output is stored in OrthoFinder/Results_DATE , where DATE is in format: Nov08 for 08 th November
         # or in this case OrthoFinder/Results_DATE_basename(proteome_folder)
