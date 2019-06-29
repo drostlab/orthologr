d0d761ac5dfb929613d259f13d6c7001607e7ea7
HajkD <hajkdrost@googlemail.com>
2019-06-29 13:26:26 +0100

    update unit test

--- tests/testthat/test-blast.R
+++ tests/testthat/test-blast.R
@@ -1,1 +1,13 @@
 context("Test: blast()")
+
+test_that(
+        "blast() runs properly ...",
+        {
+                test_blast <- blast(query_file   = system.file('seqs/ortho_thal_cds.fasta', package = 'orthologr'),
+                                    subject_file = system.file('seqs/ortho_lyra_cds.fasta', package = 'orthologr'))
+                
+                expect_true(tibble::is_tibble(test_blast))
+                expect_equal(names(test_blast), c())
+                expect_true(test_blast$perc_identity[1] == 74.0)
+        }
+)
