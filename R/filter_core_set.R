d87fbaedcf367e867178b19897956d990fb79eba
HajkD <hgd23@cam.ac.uk>
2019-07-04 16:48:19 +0100

    adding new helper function for core orthologous set retrieval

---
+++ R/filter_core_set.R
@@ -0,0 +1,43 @@
+#' #' @title Helper function to extract a core set of orthologous gene loci
+filter_core_set <- function(x, order_species) {
+        if (identical(
+                sort(as.character(unique(names(table(
+                        x$subject_species
+                ))))),
+                sort(order_species)
+        )) {
+                return(x)
+        } else {
+                return(
+                        tibble::tibble(
+                                query_species = NA,
+                                subject_species = NA,
+                                query_id = NA,
+                                query_gene_locus_id = NA,
+                                subject_id = NA,
+                                subject_gene_locus_id = NA,
+                                dN = NA,
+                                dS = NA,
+                                dNdS = NA,
+                                evalue = NA,
+                                bit_score = NA,
+                                perc_identity = NA,
+                                num_ident_matches = NA,
+                                alig_length = NA,
+                                mismatches = NA,
+                                gap_openings = NA,
+                                n_gaps = NA,
+                                pos_match = NA,
+                                ppos = NA,
+                                q_start = NA,
+                                q_end = NA,
+                                q_len = NA,
+                                qcov = NA,
+                                qcovhsp = NA,
+                                s_start = NA,
+                                s_end = NA
+                        )
+                )
+
+        }
+}
