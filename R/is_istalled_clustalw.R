b8ab3fcf16c99ac498dd44d618511c58a9220daf
HajkD <hgd23@cam.ac.uk>
2019-07-05 14:21:47 +0100

    Fixing bug in which is_installed_clustal() falsely returns non-installed clustal for Windows users #12 (Many thanks to Milan Dragićević)

--- R/is_istalled_clustalw.R
+++ R/is_istalled_clustalw.R
@@ -1,8 +1,18 @@
 is_installed_clustalw <- function() {
         # test if a valid BLAST version is installed
+        operating_sys <- Sys.info()[1]
+        
+        if (operating_sys != "Windows") {
+                call_clustalw <- "clustalw2"
+                
+        }
+        
+        if (operating_sys == "Windows")
+                call_clustalw <- "clustalw2.exe"
+        
         tryCatch({
                 sys_out <-
-                        system("clustalw -help", intern = TRUE)
+                        system(paste0(call_clustalw, " -help"), intern = TRUE)
         }, error = function(e)
                 stop(
                         "It seems like you don't have ClustalW installed locally on your machine or the PATH variable to the ClustalW program is not set correctly.",
