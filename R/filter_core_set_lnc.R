d70ead7b6e436404a82b69a4f21837ba03d55bff
HajkD <hgd23@cam.ac.uk>
2019-08-19 18:19:17 +0100

    add new helper function

---
+++ R/filter_core_set_lnc.R
@@ -0,0 +1,40 @@
+#' @title Helper function to extract a core set of orthologous lncRNAs
+filter_core_set_lnc <- function(x, order_species) {
+        
+        subset_species <- sort(as.character(names(table(
+                x$species
+        ))))
+        general_species <- sort(order_species)
+        
+        if (identical(subset_species, general_species)) {
+                return(x)
+        } else {
+                return(
+                        tibble::tibble(
+                                species = NA,
+                                query_id = NA,
+                                subject_id = NA,
+                                perc_identity = NA,
+                                num_ident_matches = NA,
+                                alig_length = NA,
+                                mismatches = NA,
+                                gap_openings = NA,
+                                n_gaps = NA,
+                                pos_match = NA,
+                                ppos = NA,
+                                q_start = NA,
+                                q_end = NA,
+                                q_len = NA,
+                                qcov = NA,
+                                qcovhsp = NA,
+                                s_start = NA,
+                                s_end = NA,
+                                s_len = NA,
+                                evalue = NA,
+                                bit_score = NA,
+                                score_raw = NA
+                        )
+                )
+                
+        }
+}
