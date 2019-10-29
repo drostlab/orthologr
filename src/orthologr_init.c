35a25ef122cf647ba14d5ae9d896edfd3eda2a7d
HajkD <hajkdrost@googlemail.com>
2019-10-29 17:22:57 +0100

    update

--- src/orthologr_init.c
+++ src/orthologr_init.c
@@ -4,8 +4,8 @@
 #include <R_ext/Rdynload.h>
 
 /* FIXME: 
-Check these declarations against the C/Fortran source code.
-*/
+ Check these declarations against the C/Fortran source code.
+ */
 
 /* .Call calls */
 extern SEXP _orthologr_ambigousNucleotides(SEXP);
