c75c5314d09193201a63fff33161eaeede8dcbb2
HajkD <hajkdrost@googlemail.com>
2019-12-03 19:14:12 +0100

    add pre-defined col_types to import_ortholog_tables_all()

--- R/import_ortholog_tables_all.R
+++ R/import_ortholog_tables_all.R
@@ -12,7 +12,43 @@ import_ortholog_tables_all <- function(ortholog_tables_folder) {
         
         ortholog_tables <- file.path(ortholog_tables_folder, list.files(ortholog_tables_folder))
         
-        res <- suppressMessages(dplyr::bind_rows(lapply(ortholog_tables, function(x) readr::read_delim(x, col_names = TRUE, delim = ";"))))
+        res <-
+                suppressMessages(dplyr::bind_rows(lapply(ortholog_tables, function(x)
+                        readr::read_delim(
+                                x,
+                                col_names = TRUE,
+                                delim = ";",
+                                col_types = readr::cols(
+                                        "query_species" = readr::col_character(),
+                                        "subject_species" = readr::col_character(),
+                                        "query_id" = readr::col_character(),
+                                        "query_gene_locus_id" = readr::col_character(),
+                                        "subject_id" = readr::col_character(),
+                                        "subject_gene_locus_id" = readr::col_character(),
+                                        "dN" = readr::col_double(),
+                                        "dS"  = readr::col_double(),
+                                        "dNdS" = readr::col_double(),
+                                        "evalue" = readr::col_double(),
+                                        "bit_score" = readr::col_double(),
+                                        "perc_identity" = readr::col_double(),
+                                        "num_ident_matches" = readr::col_double(),
+                                        "alig_length" = readr::col_integer(),
+                                        "mismatches" = readr::col_integer(),
+                                        "gap_openings" = readr::col_integer(),
+                                        "n_gaps" = readr::col_double(),
+                                        "pos_match" = readr::col_double(),
+                                        "ppos" = readr::col_double(),
+                                        "q_start" = readr::col_integer(),
+                                        "q_end" = readr::col_integer(),
+                                        "q_len" = readr::col_double(),
+                                        "qcov" = readr::col_double(),
+                                        "qcovhsp" = readr::col_double(),
+                                        "s_start" = readr::col_integer(),
+                                        "s_end" = readr::col_integer(),
+                                        "s_len" = readr::col_double(),
+                                        "score_raw" = readr::col_double()
+                                )
+                        ))))
         message("Import was successful!")
         return(res)
 }
